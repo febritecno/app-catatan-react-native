0edfe9b0726ef58ea63c110d72db72cc0b988aa7
febritecno <febritecno0@yahoo.com>
2018-02-24 22:08:26 +0700

    init

---
+++ app/lib/Typography.js
@@ -0,0 +1,16 @@
+
+export const Typo = {
+  toolbarTitle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 18
+  },
+  cardTitle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 16
+  },
+  cardDescription: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 14,
+    lineHeight: 20
+  }
+}
